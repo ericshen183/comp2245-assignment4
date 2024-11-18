b636083b84ba74ee591be96c54c67dec6f00fad8
Eric Shen <ericshen183@gmail.com>
2024-11-17 22:23:40 -0400

    Added app.js

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-window.addEventListener("DOMContentLoaded", function(){
-    const btn = document.getElementById('searchButton');
-    btn.addEventListener("click", function() {
-        fetch("http://localhost/comp2245-assignment4/superheroes.php")
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.getElementById('searchButton');
+    btn.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        let searchVal = document.getElementById('searchBar').value.trim();
+        let cleanUrl = `superheroes.php?query=${encodeURIComponent(searchVal)}`;
+
+        fetch(cleanUrl, { method: 'GET' })
             .then(response => response.text())
-            .then(data => {
-                alert(data)
+            .then(elements => {
+                let resultDiv = document.getElementById('result');
+                let h3El = document.createElement("h2");
+                let h3Text = document.createTextNode("RESULT");
+                h3El.appendChild(h3Text);
+                let hrEl = document.createElement("hr");
+
+                resultDiv.innerHTML = '';
+                resultDiv.innerHTML = elements;
+                resultDiv.prepend(h3El, hrEl);
             })
             .catch(error => {
-                alert(error);
+                console.error('Error fetching data:', error);
+                alert('Error fetching data: ' + error.message);
             });
     });
 });
+
+
+
+    
